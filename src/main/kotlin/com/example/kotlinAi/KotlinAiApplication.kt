a428e8530f23c4b5587bd48c07dec0309757dbc2
minjun7283 <minjun7283@naver.com>
2024-07-11 14:06:02 +0900

    init project

---
+++ src/main/kotlin/com/example/kotlinAi/KotlinAiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinAi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinAiApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinAiApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kotlinAi/KotlinAiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinAi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinAiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
